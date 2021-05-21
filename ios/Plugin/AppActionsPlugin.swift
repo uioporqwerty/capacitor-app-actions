8d2fb584ec413665e95cee48c3a9d0a3bb63f491
uioporqwerty <nitish.sachar@protonmail.com>
2021-05-20 21:05:11 -0700

    add initial ios implementation for showing app action

--- ios/Plugin/AppActionsPlugin.swift
+++ ios/Plugin/AppActionsPlugin.swift
@@ -1,13 +1,33 @@
 import Foundation
 import Capacitor
 
+public struct AppAction {
+    let id: String
+    let title: String
+}
+
 @objc(AppActionsPlugin)
 public class AppActionsPlugin: CAPPlugin {
     private let implementation = AppActions()
 
     @objc func set(_ call: CAPPluginCall) {
-        if let actions = call.getArray("actions")?.capacitor.replacingNullValues() as? [String?] {
-            print(actions)
+        //TODO: Improve based on this: https://capacitorjs.com/docs/core-apis/data-types#arrays
+        if let actions = call.getArray("actions", AnyObject.self) {
+            var shortcutItems: [UIApplicationShortcutItem] = []
+            for action in actions {
+                var icon: UIApplicationShortcutIcon?
+                if action["icon"] != nil {
+                    icon = UIApplicationShortcutIcon(systemImageName: action["icon"] as! String)
+                }
+                
+                let shortcutItem = UIApplicationShortcutItem(type: action["id"]! as! String, localizedTitle: action["title"]! as! String, localizedSubtitle: action["subtitle"] as? String, icon: icon)
+                
+                shortcutItems.append(shortcutItem)
+            }
+            
+            DispatchQueue.main.async {
+                UIApplication.shared.shortcutItems = shortcutItems
+            }
         }
         
         call.resolve()
