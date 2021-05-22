5bc9cedd42c8c4f82aadd5ab0a8bcf5611bcd4ee
uioporqwerty <nitish.sachar@protonmail.com>
2021-05-22 12:31:26 -0700

    add documentation

--- src/app-action.ts
+++ src/app-action.ts
@@ -1,6 +1,13 @@
 export interface AppAction {
+    /**
+     * Unique identifier across all app actions.
+     */
     id: string
     title: string
     subtitle?: string
+    /**
+     * iOS: icon name for the system icon e.g. "star.fill"
+     * Android: icon name for the system icon e.g. "ic_menu_search"
+     */
     icon?: string
 }
--- src/definitions.ts
+++ src/definitions.ts
@@ -6,6 +6,13 @@ export interface AddAppActionOptions {
 }
 
 export interface AppActionsPlugin {
+  /**
+   * Set app actions for the app. Overrides any existing app actions.
+   */
   set(options: AddAppActionOptions): Promise<void>
+
+  /**
+   * Listen for when an app action has been clicked by the user. actionId should match existing app actions.
+   */
   addListener(actionId: string, listener: (info: any) => void) : Promise<PluginListenerHandle> & PluginListenerHandle
 }
